9d8ef3148aeda846dc58a5f5bc2de69eab2d8135
saurav-chhetri <118334601+saurav-chhetri@users.noreply.github.com>
2023-06-15 15:30:27 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -3,40 +3,53 @@
  int main()
 
 {
-    int n,bt[30],wait_t[30],turn_ar_t[30],av_wt_t=0,avturn_ar_t=0,i,j;
-    printf("Please enter the total number of processes(maximum 30):");  
+    int n,bt[30],at[30],bts[30]={0},wait_t[30],turn_ar_t[30],av_wt_t=0,avturn_ar_t=0,i,j;
+    printf("Please enter the total number of processes(maximum 30):");  // the maximum process that be used to calculate is specified.
     scanf("%d",&n);
 
     printf("\nEnter The Process Burst Time");
-    for(i=0;i<n;i++)  
+    for(i=0;i<n;i++)  // burst time for every process will be taken as input
     {
-        printf("P[%d]:",i+1);
+        printf("P[%d]:",i);
         scanf("%d",&bt[i]);
     }
-
-    wait_t[0]=0;
-
-    for(i=1;i<n;i++)
+     printf("\nEnter The Arrival Time");
+    for(i=0;i<n;i++)  // burst time for every process will be taken as input
     {
-        wait_t[i]=0;
-        for(j=0;j<i;j++)
-            wait_t[i]+=bt[j];
+        printf("AT[%d]:",i);
+        scanf("%d",&at[i]);
     }
-
-    printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
-
     for(i=0;i<n;i++)
     {
-        turn_ar_t[i]=bt[i]+wait_t[i];
-        av_wt_t+=wait_t[i];
-        avturn_ar_t+=turn_ar_t[i];
-        printf("\nP[%d]\t\t%d\t\t\t%d\t\t\t\t%d",i+1,bt[i],wait_t[i],turn_ar_t[i]);
+
+        for(j=0;j<=i;j++){
+            bts[i] += bt[j];
+        }
+
     }
+    for(i=0;i<n;i++){
+        printf("%d ",bts[i]);
+    }
+    int tat[30];
+    for(i=0;i<n;i++){
+        tat[i] = bts[i] - at[i];
+    }
+    float tat_avg = 0;
+    for(i=0;i<n;i++){
+        tat_avg += tat[i];
+    }
+    tat_avg = tat_avg/n;
+    printf("\n%f",tat_avg);
+    int awt[30];
+    for(i=0;i<n;i++){
+        awt[i] = tat[i] - bt[i];
+    }
+    float awt_avg=0;
+    for(i=0;i<n;i++){
+        awt_avg+= awt[i];
+    }
+    awt_avg = awt_avg/n;
+    printf("\n%f",awt_avg);
 
-    av_wt_t/=i;
-    avturn_ar_t/=i;  
-    printf("\nAverage Waiting Time:%d",av_wt_t);
-    printf("\nAverage Turnaround Time:%d",avturn_ar_t);
 
-    return 0;
 }
